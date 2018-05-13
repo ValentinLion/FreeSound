fa74ff7812bb04802ee0d52bc1ae1f26303c617d
valentin <valentin.lion@laposte.net>
2018-05-13 16:37:21 +0200

    Calcule du MFCC Affichage d'un descriptif des données

--- Test.py
+++ Test.py
@@ -1,32 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import numpy as np
-import os
 import pandas as pd
 
+import description
+import submission
+
 df_training = pd.read_csv("train.csv", sep=",")
+df_test = pd.read_csv("sample_submission.csv", sep=",")
 
-list_test = os.listdir("/home/valentin/Téléchargements/audio_test")
+submission.fake_submission(df_training, df_test)
+print("Finish")
 
-labels = df_training.label.unique()
+description.description(df_training)
 
-proba = []
-
-for label in labels:
-    print("label:" + label)
-    print((df_training.label == label).sum())
-    print((df_training.label == label).mean())
-    proba.append((df_training.label == label).mean())
-
-soumission_proba = []
-
-for i in range(0, len(list_test)):
-    soumission_proba.append(
-        np.random.choice(labels, p=proba) + " " + np.random.choice(labels, p=proba) + " " + np.random.choice(labels,
-                                                                                                             p=proba))
-
-results = pd.DataFrame({'fname': list_test})
-results["label"] = soumission_proba
-
-results.to_csv(path_or_buf="predict.csv", sep=',', index=False)
+print("Finish")
---
+++ description.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import librosa
+import librosa.display
+
+
+def description(df_training):
+    # Labels
+    labels = df_training.label.unique()
+    print "Number of labels : " + str(len(labels))
+    print labels
+
+    # Missing data
+    missing_data = df_training.isnull().sum().sort_values(ascending=False)
+    tab_missing_data = pd.concat([missing_data], axis=1, keys=["Total"])
+    print(tab_missing_data)
+
+    # Manually Verified
+    data_manually_verified = df_training["manually_verified"].value_counts();
+    labels_manually_verified = data_manually_verified.index
+
+    plt.pie(data_manually_verified, labels=labels_manually_verified, startangle=90, autopct='%.1f%%')
+    plt.title("Data manually verified 0:No 1:Yes")
+    plt.show()
+
+    # MFCC
+
+    for row in df_training.iterrows():
+
+        if (row[1].label == 'Knock'):
+            file_path = "/home/valentin/Téléchargements/audio_train/" + row[1].fname
+            y, sr = librosa.load(file_path)
+
+            mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
+
+            plt.figure(figsize=(10, 4))
+            librosa.display.specshow(mfccs, x_axis='time')
+            plt.colorbar()
+            plt.title('MFCC')
+            plt.tight_layout()
+            plt.show()
