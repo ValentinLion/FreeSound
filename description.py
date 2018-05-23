cde4e68a67ec48aaab7a1550a3088c544c83fe7d
valentin <valentin.lion@laposte.net>
2018-05-23 14:29:37 +0200

    Ajout distribution label

--- description.py
+++ description.py
@@ -12,11 +12,23 @@ def description(df_training):
     # Labels
     labels = df_training.label.unique()
     print "Number of labels : " + str(len(labels))
-    print labels
+
+    # display distribution
+    df_display = df_training[["fname", "label", "manually_verified"]]
+
+    category_group = df_display.groupby(['label', 'manually_verified']).count()
+    plot = category_group.unstack().reindex(category_group.unstack().sum(axis=1).sort_values().index) \
+        .plot(kind='bar', stacked=True, title="Nombre d'echantillons selon le type de son")
+    plot.set_xlabel("Type")
+    plot.set_ylabel("Nombre")
+    plt.show()
+
 
     # Missing data
     missing_data = df_training.isnull().sum().sort_values(ascending=False)
     tab_missing_data = pd.concat([missing_data], axis=1, keys=["Total"])
+
+    print("Missing Data")
     print(tab_missing_data)
 
     # Manually Verified
@@ -26,20 +38,3 @@ def description(df_training):
     plt.pie(data_manually_verified, labels=labels_manually_verified, startangle=90, autopct='%.1f%%')
     plt.title("Data manually verified 0:No 1:Yes")
     plt.show()
-
-    # # MFCC
-    #
-    # for row in df_training.iterrows():
-    #
-    #     if (row[1].label == 'Knock'):
-    #         file_path = "/home/valentin/Téléchargements/audio_train/" + row[1].fname
-    #         y, sr = librosa.load(file_path)
-    #
-    #         mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
-    #
-    #         plt.figure(figsize=(10, 4))
-    #         librosa.display.specshow(mfccs, x_axis='time')
-    #         plt.colorbar()
-    #         plt.title('MFCC')
-    #         plt.tight_layout()
-    #         plt.show()
