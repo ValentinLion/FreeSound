fce1113e3e33d6e9937e15428b01cd10879fa395
valentin <valentin.lion@laposte.net>
2018-05-24 08:32:52 +0200

    Delete old files

--- evaluation.py
+++
@@ -1,8 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-
-
-def RandomForest(X_train, y_train, X_test, y_test):
-    rfc = RandomForestClassifier(n_estimators=150)
-    rfc.fit(X_train, y_train)
-
-    print(rfc.score(X_test, y_test))
