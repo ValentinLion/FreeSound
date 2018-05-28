1731097d4f72833699db68bdaeafccb661c21bb9
cxfreeze <corentin.roche@etudiant.univ-rennes1.fr>
2018-05-28 16:29:36 +0200

    fix

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ path_audio_test = "../FreeSoundData/audio_test/"
 #description.description(pd.read_csv("train.csv", sep=","))
 
 if (useAudioAnalysCSV):
-    df_dataframe = pd.read_csv("train_analys.csv", sep=",")
-    df_test = pd.read_csv("sample_submission_analys.csv", sep=",")
+    df_dataframe = pd.read_csv("csv/train_analys.csv", sep=",")
+    df_test = pd.read_csv("csv/sample_submission_analys.csv", sep=",")
 
 else:
-    df_dataframe = pd.read_csv("train.csv", sep=",")
+    df_dataframe = pd.read_csv("csv/train.csv", sep=",")
     df_dataframe = audio_processing.apply_audio_analys(df_dataframe, path_audio_train, rate)
 
-    df_dataframe.to_csv("train_analys.csv", sep=",")
+    df_dataframe.to_csv("csv/train_analys.csv", sep=",")
 
-    df_test = pd.read_csv("sample_submission.csv", sep=",")
+    df_test = pd.read_csv("cvs/sample_submission.csv", sep=",")
     df_test = audio_processing.apply_audio_analys(df_test, path_audio_test, rate)
 
-    df_test.to_csv("sample_submission_analys.csv", sep=",")
+    df_test.to_csv("csv/sample_submission_analys.csv", sep=",")
 
 df_dataframe_without_fname_manually_verified = df_dataframe.drop('fname', axis=1).drop('manually_verified', axis=1)
 df_test_without_fname = df_test.drop('fname', axis=1)
