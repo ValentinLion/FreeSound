0fb8a52d84570e1ffb3994aa33738d2c4834fdf4
cxfreeze <corentin.roche@etudiant.univ-rennes1.fr>
2018-05-28 16:22:37 +0200

    clean

---
+++ audio_processing.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+import librosa
+import numpy as np
+from tqdm import tqdm
+
+tqdm.pandas()
+
+
+def get_fft(name, path, rate):
+    n_fft = 20
+
+    file, _ = librosa.core.load(path + name, sr=rate)
+
+    if (len(file) != 0):
+        fft = librosa.stft(file, n_fft=n_fft)
+        return pd.Series(np.std(fft, axis=1))
+
+    return pd.Series([0] * ((n_fft / 2) + 1))
+
+def get_mfcc(name, path, rate):
+    n_mfcc = 20
+
+    file, _ = librosa.core.load(path + name, sr=rate)
+
+    if (len(file) != 0):
+        mfcc = librosa.feature.mfcc(file, sr=rate, n_mfcc=n_mfcc)
+        ft2 = librosa.feature.zero_crossing_rate(file)[0]
+        ft3 = librosa.feature.spectral_rolloff(file)[0]
+        ft4 = librosa.feature.spectral_centroid(file)[0]
+        ft5 = librosa.feature.spectral_contrast(file)[0]
+        ft6 = librosa.feature.spectral_bandwidth(file)[0]
+        ft1_trunc = np.hstack((np.mean(mfcc, axis=1), np.std(mfcc, axis=1)))
+        ft2_trunc = np.hstack((np.mean(ft2), np.std(ft2)))
+        ft3_trunc = np.hstack((np.mean(ft3), np.std(ft3)))
+        ft4_trunc = np.hstack((np.mean(ft4), np.std(ft4)))
+        ft5_trunc = np.hstack((np.mean(ft5), np.std(ft5)))
+        ft6_trunc = np.hstack((np.mean(ft6), np.std(ft6)))
+        return pd.Series(np.hstack((ft1_trunc, ft2_trunc, ft3_trunc, ft4_trunc, ft5_trunc, ft6_trunc)))
+
+    return pd.Series([0] * n_mfcc+5 *2)
+
+
+def apply_audio_analys(df, path, rate):
+    features_mfcc = pd.DataFrame(df['fname'].progress_apply(get_mfcc, path=path, rate=rate))
+    # features_stft = pd.DataFrame(df['fname'].progress_apply(get_fft, path=path, rate=rate))
+
+    # return pd.concat([df, features_mfcc, features_stft], axis=1)
+    return pd.concat([df, features_mfcc], axis=1)
---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+import audio_processing
+import description
+import evaluation
+
+useAudioAnalysCSV = False
+rate = 44100
+
+path_audio_train = "../FreeSoundData/audio_train/"
+path_audio_test = "../FreeSoundData/audio_test/"
+
+#description.description(pd.read_csv("train.csv", sep=","))
+
+if (useAudioAnalysCSV):
+    df_dataframe = pd.read_csv("train_analys.csv", sep=",")
+    df_test = pd.read_csv("sample_submission_analys.csv", sep=",")
+
+else:
+    df_dataframe = pd.read_csv("train.csv", sep=",")
+    df_dataframe = audio_processing.apply_audio_analys(df_dataframe, path_audio_train, rate)
+
+    df_dataframe.to_csv("train_analys.csv", sep=",")
+
+    df_test = pd.read_csv("sample_submission.csv", sep=",")
+    df_test = audio_processing.apply_audio_analys(df_test, path_audio_test, rate)
+
+    df_test.to_csv("sample_submission_analys.csv", sep=",")
+
+df_dataframe_without_fname_manually_verified = df_dataframe.drop('fname', axis=1).drop('manually_verified', axis=1)
+df_test_without_fname = df_test.drop('fname', axis=1)
+
+X, y, i2c = evaluation.transformLabel(df_dataframe_without_fname_manually_verified)
+preds = evaluation.randomForestPredictions(X, y, df_test_without_fname, i2c)
+
+df_submission = pd.DataFrame()
+df_submission['fname'] = df_test['fname']
+df_submission['label'] = preds
+df_submission.to_csv("submission.csv", index=False)
+
+print("Finish")
